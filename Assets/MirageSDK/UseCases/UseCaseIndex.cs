a6619730b2bf3791d0e749b5bc200b6b064794b0
LUDO\Ludovic <ludovic.domingues96@gmail.com>
2022-02-03 11:39:11 +0100

    Fixed some scene change issues

---
+++ Assets/MirageSDK/UseCases/UseCaseIndex.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WalletConnectSharp.Core;
+using WalletConnectSharp.Unity;
+
+namespace MirageSDK.UseCases
+{
+    public class UseCaseIndex : MonoBehaviour
+    {
+
+        [SerializeField]
+        private GameObject _loginButton;
+        [SerializeField]
+        private GameObject _useCaseButtonsMenu;
+
+        void Start() {
+            if(WalletConnect.ActiveSession.Connected) {
+                EnableUseCaseMenu();
+            }
+
+        }
+
+        private void EnableUseCaseMenu() {
+            _loginButton.SetActive(false);
+            _useCaseButtonsMenu.SetActive(true);
+        }
+
+    }
+
+}
